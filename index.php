da6b4a2c8a8f8c36fe9befb3f3ed2e8e6811f231
hayatulhabirun88 <hayatulhabirun@gmail.com>
2018-08-19 17:37:16 +0800

    ini uji coba juga

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Skripsi Online</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Selamat datang di Skripsi online</h1>
+	<nav>	
+		Home | Judul Skripsi | Download  
+	</nav>
+	<main>	
+		<div class="container">	
+			<div class="row">
+				<div class="col-sm-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis possimus non quod beatae, deserunt tempora omnis cupiditate, sunt dolor harum aliquam amet, totam odio corporis voluptate quae saepe incidunt ipsam.</div>
+				<div class="col-sm-9">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore praesentium enim rerum totam iure deleniti eveniet sequi, similique quos commodi minima maxime, aspernatur adipisci quis qui numquam at nostrum quia.</div>
+			</div>
+		</div>
+	</main>
+	<footer>	
+		<p>@hayatul habirun</p>
+	</footer>
+</body>
+</html>
